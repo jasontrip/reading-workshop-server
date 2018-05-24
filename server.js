f03c924a788b596f1d294343746b910250ca4921
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-05-24 12:54:08 -0700

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
 const PORT = process.env.PORT || 8080;
+
+app.use(cors({origin: CLIENT_ORIGIN}));
 
 app.get('/api/*', (req, res) => {
 	res.json({ok: true});
