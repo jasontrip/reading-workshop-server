188e431c2fd9260bfd512a5bdb8284ee97a13732
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-08-24 15:24:25 -0700

    use http also

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
+const baseUrl = 'reading-workshop-client.netlify.com';
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://reading-workshop-client.netlify.com'
+  ? [`https://${baseUrl}`, `http://${baseUrl}`]
   : 'http://localhost:3000';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/reading-workshop';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
